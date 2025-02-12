2c04847e88e552a37cc029af8b87bdd31fbaf865
hebotkr <heprabathmdacc@gmail.com>
2025-02-12 12:46:28 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,7 +11,7 @@ cmd({
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
     await robin.sendPresenceUpdate('recording', from);
-    await robin.sendMessage(from, { audio: { url: "https://github.com/hebotkr/KAVI-MD-/raw/refs/heads/main/ASLAVA%20FUNK!%20(1).mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+    await robin.sendMessage(from, { audio: { url: "https://github.com/hebotkr/HELP/raw/refs/heads/main/ASLAVA%20FUNK!%20(1).mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
