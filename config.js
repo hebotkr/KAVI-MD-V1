c8db1b3f446dc9cff9cbb3228abe2761d55de2e3
hebotkr <heprabathmdacc@gmail.com>
2025-02-12 12:51:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "cwYVCLDD#KFqWF2_vy3CDIQNEBoJ_8AKEXUayuBoY2UYXsMfWDKo",
   OWNER_NUM: process.env.OWNER_NUM || "94762858448",
   PREFIX: process.env.PREFIX || ".",
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/hebotkr/KAVI-MD-/blob/main/IMG-20250205-WA0002.jpg",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/hebotkr/HELP/blob/main/photo/IMG-20250205-WA0002.jpg",
   ALIVE_MSG: process.env.ALIVE_MSG || "*yes I'm Alive noW!* 🙈\n\n> 𝐌𝐚𝐝𝐞 𝐛𝐲 *𝐊𝐀𝐕𝐈𝐃𝐔 𝐑𝐀𝐒𝐀𝐍𝐆𝐀*  🎗️",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
   MODE: process.env.MODE || "private",
