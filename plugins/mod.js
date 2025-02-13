ea6e5042bd7f57411a5bc8a2d5c186a44afaddd2
hebotkr <heprabathmdacc@gmail.com>
2025-02-13 20:36:50 +0530

    Create mod.js

---
+++ plugins/mod.js
@@ -0,0 +1,38 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+cmd({
+    pattern: "mod",
+    react: "🙁",
+    desc: "get modwhatsapp.",
+    category: "main",
+    filename: __filename
+},
+
+async function modwhatsapp(q) {
+  try {
+    	const url = `https://apkdon.net/?s=${q}`;
+const response = await axios.get(url);  
+const $ = cheerio.load(response.data);
+   
+    const results = [];
+    $("article").each((c, d) => {
+
+        results.push({
+             
+         link: $(d).find("h2 > a").attr("href"),
+         title: $(d).find("h2.entry-title").text()
+          
+
+         
+        })
+    })
+
+    return results;
+  } catch (error) {
+    console.error('Error fetching search results:', error);
+    return [];
+  }
+}
+
+module.exports = { modwhatsapp }
